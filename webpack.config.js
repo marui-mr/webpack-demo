84ef15157615f28fbc95ec1598c4432f5172ea98
marui-mr <ma1271968407@163.com>
2021-06-22 16:50:23 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 var path = require("path");
 
 module.exports = {
@@ -6,5 +7,19 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "写代码啦",
+      template: "src/assets/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
 };
